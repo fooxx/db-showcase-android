982ebd2438e9c484c249085772cf372766dd0500
Michal Jenicek <michal.jenicek@strv.com>
2016-11-12 13:02:33 +0100

    Add student item to interaction card.

--- mobile/src/main/java/cz/koto/misak/dbshowcase/android/mobile/ui/interaction/InteractionItemViewModel.java
+++ mobile/src/main/java/cz/koto/misak/dbshowcase/android/mobile/ui/interaction/InteractionItemViewModel.java
@@ -1,12 +1,20 @@
 package cz.koto.misak.dbshowcase.android.mobile.ui.interaction;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.koto.misak.dbshowcase.android.mobile.BR;
 import cz.koto.misak.dbshowcase.android.mobile.R;
 import cz.koto.misak.dbshowcase.android.mobile.model.SchoolClassInterface;
+import cz.koto.misak.dbshowcase.android.mobile.model.StudentInterface;
+import cz.koto.misak.dbshowcase.android.mobile.model.TeacherInterface;
+import me.tatarka.bindingcollectionadapter.ItemView;
 
 
 public class InteractionItemViewModel {
 
+	public final ItemView studentItemView = ItemView.of(BR.studentViewModel, R.layout.item_interaction_student);
 	private SchoolClassInterface mSchoolModelItem;
 
 
@@ -26,4 +34,17 @@ public class InteractionItemViewModel {
 		return mSchoolModelItem.getName();
 	}
 
+
+	public List<StudentInterface> getStudentList() {
+		if(mSchoolModelItem == null)
+			return new ArrayList<>();
+		return mSchoolModelItem.getStudentList();
+	}
+
+
+	public List<TeacherInterface> getTeacherList() {
+		if(mSchoolModelItem == null)
+			return new ArrayList<>();
+		return mSchoolModelItem.getTeacherList();
+	}
 }
