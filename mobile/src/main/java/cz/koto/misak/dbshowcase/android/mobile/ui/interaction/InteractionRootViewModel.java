b42a268d7a30115e9d033cac51b3204dbbc92b71
Michal Jenicek <michal.jenicek@strv.com>
2017-07-05 13:11:15 +0200

    Fix stateful layout.

--- mobile/src/main/java/cz/koto/misak/dbshowcase/android/mobile/ui/interaction/InteractionRootViewModel.java
+++ mobile/src/main/java/cz/koto/misak/dbshowcase/android/mobile/ui/interaction/InteractionRootViewModel.java
@@ -3,8 +3,12 @@ package cz.koto.misak.dbshowcase.android.mobile.ui.interaction;
 import android.databinding.ObservableArrayList;
 import android.databinding.ObservableField;
 import android.databinding.ObservableList;
+import android.view.LayoutInflater;
 
+import cz.kinst.jakub.view.SimpleStatefulLayout;
+import cz.kinst.jakub.view.StatefulLayout;
 import cz.koto.misak.dbshowcase.android.mobile.BR;
+import cz.koto.misak.dbshowcase.android.mobile.R;
 import cz.koto.misak.dbshowcase.android.mobile.model.DataHandlerListener;
 import cz.koto.misak.dbshowcase.android.mobile.model.ModelProvider;
 import cz.koto.misak.dbshowcase.android.mobile.model.SchoolModel;
@@ -12,20 +16,11 @@ import cz.koto.misak.dbshowcase.android.mobile.ui.StateListener;
 import cz.koto.misak.dbshowcase.android.mobile.ui.base.BaseViewModel;
 import io.reactivex.Observable;
 import me.tatarka.bindingcollectionadapter2.OnItemBind;
-//import me.tatarka.bindingcollectionadapter.BaseItemViewSelector;
-//import me.tatarka.bindingcollectionadapter.ItemView;
-//import me.tatarka.bindingcollectionadapter.ItemViewSelector;
 
 
 public class InteractionRootViewModel extends BaseViewModel<cz.koto.misak.dbshowcase.android.mobile.databinding.FragmentInteractionRootBinding>
 		implements DataHandlerListener, StateListener {
 
-//	public final ItemViewSelector<InteractionCard> cardItemView = new BaseItemViewSelector<InteractionCard>() {
-//		@Override
-//		public void select(ItemView itemView, int position, InteractionCard item) {
-//			itemView.set(BR.viewModel, item.getPagerLayoutResource());
-//		}
-//	};
 
 
 	public final OnItemBind<InteractionCard> cardItemView = (itemBinding, position, item) -> itemBinding.set(BR.viewModel, item.getPagerLayoutResource());
@@ -34,11 +29,15 @@ public class InteractionRootViewModel extends BaseViewModel<cz.koto.misak.dbshow
 	public final ObservableField<SchoolModel> schoolModel = new ObservableField<>();
 	public final ObservableList<InteractionCard> cardItemList = new ObservableArrayList<>();
 
+	public StatefulLayout.StateController stateController = null;
 
 	@Override
 	public void onViewModelCreated() {
 		super.onViewModelCreated();
-		//TODO getBinding().stateful.showProgress();
+		stateController = StatefulLayout.StateController.create()
+				.withState(SimpleStatefulLayout.State.PROGRESS, LayoutInflater.from(getActivity()).inflate(R.layout.include_progress, null))
+				.build();
+		stateController.setState(SimpleStatefulLayout.State.PROGRESS);
 		ModelProvider.get().loadModel(this);
 	}
 
@@ -53,7 +52,7 @@ public class InteractionRootViewModel extends BaseViewModel<cz.koto.misak.dbshow
 	@Override
 	public void onResume() {
 		super.onResume();
-		//TODO getBinding().stateful.showProgress();
+		stateController.setState(SimpleStatefulLayout.State.PROGRESS);
 		ModelProvider.get().loadModel(this);
 		refreshCardItemList();
 		//TODO updateToolbar();
@@ -62,7 +61,7 @@ public class InteractionRootViewModel extends BaseViewModel<cz.koto.misak.dbshow
 
 	@Override
 	public void handleSuccess() {
-		//TODO getBinding().stateful.showContent();
+		stateController.setState(SimpleStatefulLayout.State.CONTENT);
 		schoolModel.set(ModelProvider.get().getSchoolModel());
 		refreshCardItemList();
 		//TODO updateToolbar();
@@ -71,7 +70,7 @@ public class InteractionRootViewModel extends BaseViewModel<cz.koto.misak.dbshow
 
 	@Override
 	public void handleFailed(Throwable throwable) {
-		//TODO getBinding().stateful.showContent();
+		stateController.setState(SimpleStatefulLayout.State.CONTENT);
 		schoolModel.set(ModelProvider.get().getSchoolModel());
 		refreshCardItemList();
 		//TODO updateToolbar();
@@ -80,13 +79,13 @@ public class InteractionRootViewModel extends BaseViewModel<cz.koto.misak.dbshow
 
 	@Override
 	public void setProgress() {
-		//TODO getBinding().stateful.showProgress();
+		stateController.setState(SimpleStatefulLayout.State.PROGRESS);
 	}
 
 
 	@Override
 	public void setContent() {
-		//TODO getBinding().stateful.showContent();
+		stateController.setState(SimpleStatefulLayout.State.CONTENT);
 	}
 
 
